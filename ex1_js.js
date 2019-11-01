d251e7815a432a1f9a6f0680c8d9e1a67eb3ba6c
BG629 <53467776+BG629@users.noreply.github.com>
2019-11-01 13:19:37 +0200

    Update ex1_js.js

--- ex1_js.js
+++ ex1_js.js
@@ -1,98 +1,103 @@
-$(document).ready(function(){
-    class profile{
+class profile{
 
-        constructor(id, name, gender, age, height, city, image, premium){
-            this.id = id;
-            this.name = name;
-            this.gender = gender;
-            this.age = age;
-            this.height = height;
-            this.city = city;
-            this.image = image;
-            this.premium = premium;
-        }
-    
-        Render(){
-    
-            return '<p>ID: '+ this.id +'</p><br> <p>Name: '+this.name+'</p><br> <p>Gender: '+this.gender+'</p><br> <p>Age: '+this.age+'</p><br>'+
-             '<p>Height: '+this.height+'</p><br> <p>City: '+this.city+'</p><br> <p>Image: '+this.image+'</p><br> <p>Premium: '+this.premium+'</p><br>'
-    
-        }
+    constructor(id, name, gender, age, height, location, image, premium){
+        this.id = id;
+        this.name = name;
+        this.gender = gender;
+        this.age = age;
+        this.height = height;
+        this.location = location;
+        this.image = image;
+        this.premium = premium;
     }
+
+    Render(){
+
+        return '<p>ID: '+ this.id +'</p><br> <p>Name: '+this.name+'</p><br> <p>Gender: '+this.gender+'</p><br> <p>Age: '+this.age+'</p><br>'+
+         '<p>Height: '+this.height+'</p><br> <p>location: '+this.location+'</p><br> <p>Image: '+this.image+'</p><br> <p>Premium: '+this.premium+'</p><br>'
+
+    }
+}
+
+class premium extends profile{
     
-    class premium extends profile{
+    constructor(id, name, gender, age, height, location, image, premium, hobbies){
+        super(id, name, gender, age, height, location, image, premium);
+        this.hobbies = hobbies;
+    }
+
+    Render(){ // CHECK THIS ONE ?????
+
+        let st = 'Hobbies: '
+        for (let i = 0; i < this.hobbies.length; i++) {
+            st +=  this.hobbies[i]+' ';
+        }
+
+        return super.Render() + st;
+    }
+}
+
+class MainApp{
+
+    constructor(users){
         
-        constructor(id, name, gender, age, height, city, image, premium, hobbies){
-            super(id, name, gender, age, height, city, image, premium);
-            this.hobbies = hobbies;
-        }
-    
-        Render(hobbiesArr){ // CHECK THIS ONE ?????
-    
-            st = 'Hobbies: '
-            for (let i = 0; i < hobbiesArr.length; i++) {
-                st +=  hobbiesArr[i]+' ';
+        this.profiles = [];
+        this.premiums = [];
+
+        for (let i = 0; i < users.length; i++) {
+
+            if(users[i].premium == true){ // Premium user
+                this.premiums.push(new premium(users[i].id,users[i].name,users[i].gender,users[i].age,
+                    users[i].height,users[i].location,users[i].image,users[i].premium,users[i].hobbies));
+                continue;
             }
-    
-            return this.Render() + st;
+            // Not Premium
+            // users[i] = new profile(users[i].id,users[i].name,users[i].gender,users[i].age,
+            // users[i].height,users[i].location,users[i].image,users[i].premium);
+            this.profiles.push(new profile(users[i].id,users[i].name,users[i].gender,users[i].age,
+                users[i].height,users[i].location,users[i].image,users[i].premium));
         }
+
+        console.log(this.profiles, this.premiums)
     }
+
+    Filter(minAge, maxAge, gender){  // returns filtered array of premium/profile objects
+
+        let mapped_profiles = this.profiles.filter( function (x){
+            return (x.age <= maxAge && x.age >= minAge && x.gender == gender);
+        });
+        let mapped_premiums = this.premiums.filter( function (x){
+            return (x.age <= maxAge && x.age >= minAge && x.gender == gender);
+        });
+
+        mapped_profiles.forEach( user => {
+            mapped_premiums.push(user);
+          });
+
+        return mapped_premiums;
+    }
+
+    Next(){
+
+    }
+
+    Render(){
+
+    }
+
+} 
+
+$(document).ready(function(){
     
-    class MainApp{
-    
-        constructor(userArr){
-    
-            for (let i = 0; i < userArr.length; i++) {
-    
-                if(userArr[i].premium == true){ // Premium user
-                    userArr[i] = new premium(userArr[i].id,userArr[i].name,userArr[i].gender,userArr[i].age,
-                    userArr[i].height,userArr[i].city,userArr[i].image,userArr[i].premium,userArr[i].hobbies)
-                    continue;
-                }
-                // Not Premium
-                userArr[i] = new profile(userArr[i].id,userArr[i].name,userArr[i].gender,userArr[i].age,
-                userArr[i].height,userArr[i].city,userArr[i].image,userArr[i].premium);
-            }
-
-            // userArr.forEach(element => {
-            //     if (element.premium == true) {
-            //         element = new premium(element.id,element.name,element.gender,element.age,
-            //              element.height,element.city,element.image,element.premium,element.hobbies);
-            //              continue;
-            //     }
-            //     element = new profile(element.id,element.name,element.gender,element.age,
-            //         element.height,element.city,element.image,element.premium);
-            // });
-            
-        }
-
-        Filter(){
-            var mapped_UsersArr = users.filter( function (x){
-                return (x.age <= $('#maxAge').val() && x.age >= $('#minAge').val() && x.gender == GenderSelectbtn);
-            });
-            return mapped_UsersArr;
-        }
-
-        Next(){
-
-        }
-
-        Render(){
-
-        }
-    
-    } 
-
-
     $('#submitbtn').click(function(){
         
         if (GenderSelectbtn == '' ) {
           alert("Click On Male / Female Icon");
-          return
+          return;
         }
         if ( $('#maxAge').val() == '' || $('#minAge').val() == '' ) {
             alert('Enter Min/Max age values')
-            return
+            return;
         }
         if($('#minAge').val() < 18){ alert('Are U A Pedophile??\n'); return;}
 
@@ -107,23 +112,19 @@ $(document).ready(function(){
             alert('There Is No Match')
             return;
         }
-        mainapp1 = new MainApp(users);
-        mapped_arr = mainapp1.Filter();
-        console.log(mainapp1)
-        console.log(mapped_arr)
 
-        localStorage.setItem("mapped_arr", JSON.stringify(mapped_arr));
+        conditions_arr = {"minAge":$('#minAge').val(),"maxAge":$('#maxAge').val(),"gender":GenderSelectbtn}
+        
+        localStorage.clear();
+        localStorage.setItem("conditions_arr", JSON.stringify(conditions_arr));
 
         // Switch To new window
         window.open('Users.html','_self');
-
-        
-        
     }); 
 });
 
-
-    var GenderSelectbtn = ''; // 0 == male || 1 == female
+    var mapped_arr = [];
+    var GenderSelectbtn = '';
     var users = [
         {id : 0, name : "Beyonce knowles", gender: "female", age: 37, height: 167, location: "Los Angeles",
         hobbies: ["Sing", "Dance"],
@@ -157,7 +158,12 @@ $(document).ready(function(){
     
     function UsersLoad(){
 
-        var mapped_arr = JSON.parse(localStorage.getItem("mapped_arr"));
+        var conditions_arr = JSON.parse(localStorage.getItem("conditions_arr"));
+
+        app = new MainApp(users);
+        mapped_arr = app.Filter(conditions_arr.minAge,conditions_arr.maxAge,conditions_arr.gender)
+
+        console.log(mapped_arr)
 
         $(document).ready(function(){
 
@@ -165,9 +171,9 @@ $(document).ready(function(){
 
             $('#NamePH').html('<h2 class="text-nowrap">'+mapped_arr[0].name+'</h2>');
 
-            $('#imgPH').html('<img src="'+mapped_arr[0].image+'" alt="" class="img-responsive img-thumbnail rounded mx-auto d-block">');
+            $('#imgPH').html('<img src="'+mapped_arr[0].image+'" alt="" class="img-fluid img-thumbnail rounded mx-auto">');
 
-            $('#detailsPH').html();
+            $('#detailsPH').html(mapped_arr[0].Render());
 
             $('#next').click(function(){
                 
@@ -178,7 +184,8 @@ $(document).ready(function(){
                 }
                 
                 $('#NamePH').html('<h2 class="text-nowrap">'+mapped_arr[i].name+'</h2>');
-                $('#imgPH').html('<img src="'+mapped_arr[i].image+'" alt="" class="img-responsive img-thumbnail rounded mx-auto d-block">');
+                $('#imgPH').html('<img src="'+mapped_arr[i].image+'" alt="" class="img-fluid img-thumbnail rounded mx-auto">');
+                $('#detailsPH').html(mapped_arr[i].Render());
             });
         });
   
